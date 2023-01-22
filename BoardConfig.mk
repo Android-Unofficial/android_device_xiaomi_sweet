8e1eca6a6f6e739eb9d86fe7b5eaa73032be4b4f
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2023-01-22 19:33:21 +0530

    sweet: manifests: add missing HALs * The following HALs in device manifest are not declared in FCM <= level 5:   android.hardware.drm.IDrmFactory/clearkey (@1)   android.hardware.wifi.hostapd.IHostapd/default (@1)   android.hardware.wifi.supplicant.ISupplicant/default (@1)   android.hardware.wifi@1.6::IWifi/default
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,7 @@ KERNEL_LD := LD=ld.lld
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/manifests/device_framework_matrix.xml \
     $(DEVICE_PATH)/manifests/lineage_device_framework_matrix.xml
+    $(DEVICE_PATH)/manifests/framework_compatibility__matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/manifests/compatibility_matrix.xml
 ODM_MANIFEST_SKUS := nfc
