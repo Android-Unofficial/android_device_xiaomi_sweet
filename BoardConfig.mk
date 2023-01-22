64d55daff961c2334d0e5f9b20706269cb4b54b2
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2023-01-23 01:54:03 +0530

    sweet: drop Lineage stuff * LiveDisplay * Trust HAL
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,6 @@ TARGET_KERNEL_CLANG_COMPILE := true
 
 # Manifest
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/manifests/lineage_device_framework_matrix.xml \
     $(DEVICE_PATH)/manifests/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/manifests/compatibility_matrix.xml
