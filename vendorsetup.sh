e65a7a52606e3f6c3bc7b4f6afe748f39083916b
dopaemon <polarisdp@gmail.com>
2023-02-28 11:59:35 +0000

    vendorsetup: Setup vendor/kernel/modules
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,9 +5,23 @@ if [ -d "$KERNELSU" ]; then
   rm -rf system/extras/su
 fi
 
-KPROFILEDIR="packages/apps/KProfiles/"
+KPROFILEDIR="packages/apps/KProfiles"
 if [ -d "$KPROFILEDIR" ]; then
-  echo "Installing config files in ${DIR}..."
+  echo "Installing KProfiles in ${DIR}..."
 else
   git clone https://github.com/CannedShroud/android_packages_apps_KProfiles packages/apps/KProfiles
+fi
+
+VENDORDIR="vendor/xiaomi/sweet"
+if [ -d "$VENDORDIR" ]; then
+  echo "Installing Vendor in ${DIR}..."
+else
+  git clone -b A13 --single-branch https://github.com/Android-Unofficial/android_vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+fi
+
+KERNELDIR="kernel/xiaomi/sweet"
+if [ -d "$KERNELDIR" ]; then
+  echo "Installing Kernel in ${DIR}..."
+else
+  git clone -b A13 --single-branch https://github.com/Android-Unofficial/android_kernel_xiaomi_sweet.git kernel/xiaomi/sweet
 fi
